75f0d6720c4d103b8636606dcb8389c99e8c648e
Victor <thaivu.tcnh2@gmail.com>
2023-10-28 09:15:59 +1100

    add: passport.mjs

--- server/services/passport.mjs
+++ server/services/passport.mjs
@@ -1,10 +1,12 @@
-import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20'; 
-
 import GoogleKey from '../config/key.mjs';
+import mongoose from 'mongoose';
+import passport from 'passport';
+
+const User = mongoose.model('users');
 
 // create new instance of google passport strategy
-// passport.use is a generic command
+// passport.use is a generic command to connect and perform task on database
 passport.use(
 	new GoogleStrategy({
 		clientID: GoogleKey.googleClientId,
@@ -12,9 +14,21 @@ passport.use(
 		callbackURL: "/auth/google/callback",
 		}, 
 		(accessToken, refreshToken, profile, done) => {
-			//log the accessToken
-			console.log(accessToken);
-			console.log(refreshToken);
-			console.log(profile);
+			// this returns promises aka async query
+			// check if there is user in the db
+			User.findOne({ googleId: profile.id })
+				.then((existingUser) => {
+					if (existingUser) {
+						// we already have record of this user
+						done(null, existingUser);
+					} else {
+						// create a new model instance in mongoose
+						new User({ 
+							googleId: profile.id
+						})
+							.save()
+							.then(user => done(null, user)); // this save the instance into the database 
+					}
+				})
 		}
 ));
